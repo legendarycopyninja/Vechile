7b8e2f5d39d22c32aa78d63256219285af0f5c7a
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2022-10-17 10:31:35 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,26 @@
+ 
+let count = 0;
+let result = document.getElementById("count");
+let saveEl = document.getElementById("savingresult");
+console.log(saveEl); 
+
+function increment() {
+ count++;
+result.innerText = count;
+}
+
+function reset() {
+    result.textContent = 0;
+    saveEl.textContent = "Previous Entries : " ;
+   }
+
+   function savelog() {
+   let counstr = ` ${count} - `;
+   saveEl.textContent += counstr;
+   result.textContent = 0;
+   count = 0;
+   }
+
+
+
+
